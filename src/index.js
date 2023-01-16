dbf07b8066dac347502407e837eb48d713fd1e52
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-01-16 17:08:37 +0100

    pequeños cambios en js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 window.onload = () => {
     console.log('loaded')
+    let cells = document.getElementsByClassName('cell');
 
-    // let cells = <GET ALL cell ELEMENTS>
-    let cell = document.getElementsByClassName('cell-x')[0]
-    // for (const cell of cells) {
+    let currentTurn = document.getElementsByClassName('turn-cell-o')[0];
+
+    for (const cell of cells) {
         cell.onclick = (event) => {
+            console.log(cells);
             const [, x, y] = event.target.id.split('-')
             console.log(`click on ${x}:${y}`)
-            if (cell.className === 'cell')
-                cell.className = 'cell-o'
-            else {
-                if (cell.className === 'cell-o')
-                    cell.className = 'cell-x'
-                else
-                    cell.className = 'cell-o'
+            cell.className = currentTurn.className;
+            if(currentTurn.className === 'turn-cell-x'){
+                currentTurn.className = 'turn-cell-o'
+            }else{
+                currentTurn.className = 'turn-cell-x'
             }
+            cells = document.getElementsByClassName('cell');
         }
-    // }
+    }
 }
